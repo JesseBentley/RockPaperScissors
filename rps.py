bf5b557ee3212ddbcaa8d4d95cb18554662ff3bc
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-13 22:09:46 -0400

    test

--- rps.py
+++ rps.py
@@ -2,8 +2,8 @@ from random import randint
 
 t = ["Rock", "Paper", "Scissors"]
 
-computer = t[randomint(0, 2)]
-player = false
+computer = t[randint(0, 2)]
+player = False
 while player == False:
     player = input("Rock, Paper, Scissors?")
     if player == computer:
